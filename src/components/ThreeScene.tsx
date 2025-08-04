e30c35df9a319846fddcf4bf54f6b4ceae98049a
willcordeiro <willcordeiro800@gmail.com>
2025-08-04 20:57:38 -0300

    fix update

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,38 +1,51 @@
-"use client"
+"use client";
 
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Environment } from "@react-three/drei"
-import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import { CityGrid } from "@/components/city-grid";
 
-function Box() {
-  return (
-    <mesh position={[0, 0, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  )
-}
-
-function Scene() {
+export default function Home() {
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <Box />
-      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-      <Environment preset="sunset" />
-    </>
-  )
-}
-
-export default function ThreeScene() {
-  return (
-    <div className="fixed inset-0 w-full h-full">
-      <Canvas camera={{ position: [3, 3, 3], fov: 75 }}>
+      <Canvas
+        camera={{ position: [20, 20, 20], fov: 60 }}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          background: "#777777",
+        }}
+      >
         <Suspense fallback={null}>
-          <Scene />
+          <CityGrid size={8} />
+          <ambientLight color={0xffffff} intensity={0.2} />
+
+          <directionalLight
+            color={0xffffff}
+            intensity={0.3}
+            position={[0, 1, 0]}
+          />
+          <directionalLight
+            color={0xffffff}
+            intensity={0.3}
+            position={[1, 1, 0]}
+          />
+          <directionalLight
+            color={0xffffff}
+            intensity={0.3}
+            position={[0, 1, 1]}
+          />
+
+          <OrbitControls
+            enablePan={true}
+            enableZoom={true}
+            enableRotate={true}
+          />
         </Suspense>
       </Canvas>
-    </div>
-  )
+    </>
+  );
 }
